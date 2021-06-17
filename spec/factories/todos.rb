97bff84d69bb0cec6e5b16bf92ee5c228c9c8a87
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-06-17 14:33:30 +0545

    rspec  helper and factory added

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :todo do
     title { FFaker::Lorem.word }
-    created_by { FFaker::Number.number(10) }
+    # created_by { Faker::Number.number(10) }
+    created_by { FFaker::Random.rand(1000000000..9999999999) }
   end
 end
