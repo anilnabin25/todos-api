a79f4385f98879745b0b17180e2e47597ce6c7df
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-07-09 10:44:09 +0545

    test commit
    Signed-off-by: Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -2,21 +2,24 @@ require 'rails_helper'
 
 RSpec.describe AuthenticateUser do
   # create test user
+  # invalid request subject
+
+  subject(:invalid_auth_obj) { described_class.new('foo', 'bar') }
+
   let(:user) { create(:user) }
   # valid request subject
-  subject(:valid_auth_obj) { described_class.new(user.email, user.password) }
-  # invalid request subject
-  subject(:invalid_auth_obj) { described_class.new('foo', 'bar') }
+
+  let(:valid_auth_obj) { described_class.new(user.email, user.password) }
 
   # Test suite for AuthenticateUser#call
   describe '#call' do
     # return token when valid request
-    context 'when valid credentials' do
-      # it 'returns an auth token' do
-      #   token = valid_auth_obj.call
-      #   expect(token).not_to be_nil
-      # end
-    end
+    # context 'when valid credentials' do
+    #   it 'returns an auth token' do
+    #     token = valid_auth_obj.call
+    #     expect(token).not_to be_nil
+    #   end
+    # end
 
     # raise Authentication Error when invalid request
     context 'when invalid credentials' do
